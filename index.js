08565bfa32825568a29114b8e0a0736a71b24b16
Ayoub <rouabahayoub75@gmail.com>
2026-07-22 10:13:44 +0100

    Add Render web server

--- index.js
+++ index.js
@@ -4,7 +4,22 @@ dns.setDefaultResultOrder("ipv4first");
 
 require("dotenv").config();
 
+const express = require("express");
+
+// Render Web Server
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("🐉 WesterosBot Online");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("🌐 Web Server Started");
+});
+
+
 const mongoose = require("mongoose");
+
 const { 
     Client, 
     GatewayIntentBits, 
@@ -14,11 +29,15 @@ const {
 const fs = require("fs");
 
 
+// Discord Client
 const client = new Client({
+
     intents: [
         GatewayIntentBits.Guilds
     ]
+
 });
+
 
 
 // تحميل الأوامر
@@ -31,24 +50,39 @@ const commandFiles = fs.readdirSync("./commands")
 
 for (const file of commandFiles) {
 
-    const command = require(`./commands/${file}`);
+    try {
 
-    if (command.data) {
+        const command = require(`./commands/${file}`);
 
-        client.commands.set(
-            command.data.name,
-            command
-        );
 
-        console.log(`✅ Command Loaded: ${file}`);
+        if (command.data) {
+
+            client.commands.set(
+                command.data.name,
+                command
+            );
+
+
+            console.log(`✅ Command Loaded: ${file}`);
+
+        }
+
+    } catch (error) {
+
+        console.log(`❌ Error loading ${file}`);
+        console.log(error);
+
     }
+
 }
+
 
 
 
 // تشغيل Slash Commands
 
 client.on("interactionCreate", async interaction => {
+
 
     if (!interaction.isChatInputCommand()) return;
 
@@ -63,25 +97,36 @@ client.on("interactionCreate", async interaction => {
 
     try {
 
+
         await command.execute(interaction);
 
+
     } catch (error) {
+
 
         console.error(error);
 
 
         if (!interaction.replied) {
 
+
             await interaction.reply({
+
                 content: "❌ حدث خطأ أثناء تنفيذ الأمر",
                 ephemeral: true
+
             });
+
 
         }
 
+
     }
 
+
 });
+
+
 
 
 
@@ -90,48 +135,66 @@ client.on("interactionCreate", async interaction => {
 
 async function startBot() {
 
+
     try {
 
 
         await mongoose.connect(
+
             process.env.MONGO_URI,
+
             {
                 serverSelectionTimeoutMS: 10000
             }
+
         );
 
 
         console.log("🗄️ MongoDB Connected");
 
 
+
         await client.login(
+
             process.env.TOKEN
+
         );
+
 
 
     } catch (err) {
 
 
         console.log("❌ Error:");
-
         console.log(err.message);
 
 
     }
 
+
 }
 
 
 
-// Discord.js v15 compatibility
+
+
+
+// Discord.js v15
 
 client.once("clientReady", () => {
 
+
     console.log(
+
         `🤖 WesterosBot Online as ${client.user.tag}`
+
     );
 
+
 });
+
+
+
 
 
 startBot();
